586041a792637c4e352972e02cfe68ca84ad3544
Saul Vega <saulaxios@gmail.com>
2017-07-19 18:10:42 -0500

    Span de campos obligatorios

--- js/login.js
+++ js/login.js
@@ -1,18 +1,71 @@
 const db = firebase.database();
 const auth = firebase.auth();
 
+$('#email').keyup(function () {
+  let email = $('#email').val();
+  console.log("Email");
+  if(email.length < 1) {
+    $('#email').parent().addClass('has-error');
+    $('#helpblockEmail').html("Campo obligatorio").show();
+  }
+  else {
+    $('#email').parent().removeClass('has-error');
+    $('#helpblockEmail').hide();
+  }
+});
+
+$('#contraseña').keyup(function () {
+  let contraseña = $('#contraseña').val();
+  if(contraseña.length < 1) {
+    $('#contraseña').parent().addClass('has-error');
+    $('#helpblockContraseña').html("Campo obligatorio").show();
+  }
+  else {
+    $('#contraseña').parent().removeClass('has-error');
+    $('#helpblockContraseña').hide();
+  }
+});
 
 function login() {
   let email = $('#email').val();
   let contraseña = $('#contraseña').val();
 
-  auth.signInWithEmailAndPassword(email, contraseña)
-  .then(function() { //en caso de exito
+  if(email.length > 0 && contraseña > 0) {
+    if(validarEmail(email)) {
+      auth.signInWithEmailAndPassword(email, contraseña)
+      .then(function() { //en caso de exito
 
-  })
-  .catch(function(error) { //en caso de error
-
-  });
+      })
+      .catch(function(error) { //en caso de error
+        if(error.code === 'auth/user-not-found') { //imprime un error si tu usuario es equivocado
+            $('#email').parent().addClass('has-error');
+            $('#helpblockEmail').empty().html("El usuario es incorrecto").show();
+          }
+          if(error.code === 'auth/wrong-password') { //imprime un error si te equivocaste en la contraseña
+            $('#contrasena').parent().addClass('has-error');
+            $('#helpblockContraseña').empty().html("La contraseña es incorrecta").show();
+          }
+      });
+    }
+  }
+  else {
+      if(email == "") {
+        $('#email').parent().addClass('has-error');
+        $('#helpblockEmail').html("Campo obligatorio").show();
+      }
+      else {
+        $('#email').parent().removeClass('has-error');
+        $('#helpblockEmail').hide();
+      }
+      if(contraseña == "") {
+        $('#contraseña').parent().addClass('has-error');
+        $('#helpblockContraseña').html("Campo obligatorio").show();
+      }
+      else {
+        $('#contraseña').parent().removeClass('has-error');
+        $('#helpblockContraseña').hide();
+      }
+    }
 }
 
 function obtenerUsuario() {
@@ -39,4 +92,19 @@ function obtenerUsuario() {
       $('#p').show();
     }
   });
+}
+
+function validarEmail(valor) {
+  if(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)) {
+    $('#email').parent().removeClass('has-error');
+    $('#helpblockEmail').hide();
+
+    return true;
+  }
+  else {
+    $('#email').parent().addClass('has-error');
+    $('#helpblockEmail').html("Correo no válido").show();
+
+    return false;
+  }
 }
